95cf17973f4ed865bfacf83180fdb7c65229d135
thinogueiras <thiagonogueira@hotmail.com.br>
2022-10-22 12:03:45 -0300

    Ajustes gerais no projeto

--- src/main/java/qa/thinogueiras/exceptions/FilmeSemEstoqueException.java
+++
@@ -1,6 +0,0 @@
-package qa.thinogueiras.exceptions;
-
-public class FilmeSemEstoqueException extends Exception {
-
-	private static final long serialVersionUID = -4929857957167617920L;
-}
--- src/main/java/qa/thinogueiras/utils/DataUtils.java
+++ src/main/java/qa/thinogueiras/utils/DataUtils.java
@@ -16,27 +16,11 @@ public class DataUtils {
 		calendar.add(DAY_OF_MONTH, dias);
 		return calendar.getTime();
 	}
-
-	/**
-	 * Retorna a data atual com a diferenca de dias enviados por parametro a Data
-	 * pode estar no futuro (parametro positivo) ou no passado (parametro negativo)
-	 * 
-	 * @param dias Quantidade de dias a ser incrementado/decrementado
-	 * @return Data atualizada
-	 */
+	
 	public static Date obterDataComDiferencaDias(int dias) {
 		return adicionarDias(new Date(), dias);
 	}
-
-	/**
-	 * Retorna uma instância de <code>Date</code> refletindo os valores passados por
-	 * parametro
-	 * 
-	 * @param dia
-	 * @param mes
-	 * @param ano
-	 * @return
-	 */
+	
 	public static Date obterData(int dia, int mes, int ano) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.set(DAY_OF_MONTH, dia);
@@ -44,15 +28,7 @@ public class DataUtils {
 		calendar.set(YEAR, ano);
 		return calendar.getTime();
 	}
-
-	/**
-	 * Verifica se uma data é igual a outra Esta comparação considera apenas dia,
-	 * mes e ano
-	 * 
-	 * @param data1
-	 * @param data2
-	 * @return
-	 */
+	
 	public static boolean isMesmaData(Date data1, Date data2) {
 		Calendar calendar1 = Calendar.getInstance();
 		calendar1.setTime(data1);
@@ -61,15 +37,7 @@ public class DataUtils {
 		return (calendar1.get(DAY_OF_MONTH) == calendar2.get(DAY_OF_MONTH))
 				&& (calendar1.get(MONTH) == calendar2.get(MONTH)) && (calendar1.get(YEAR) == calendar2.get(YEAR));
 	}
-
-	/**
-	 * Verifica se uma determinada data é o dia da semana desejado
-	 * 
-	 * @param data      Data a ser avaliada
-	 * @param diaSemana <code>true</code> caso seja o dia da semana desejado,
-	 *                  <code>false</code> em caso contrário
-	 * @return
-	 */
+	
 	public static boolean verificarDiaSemana(Date data, int diaSemana) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(data);
