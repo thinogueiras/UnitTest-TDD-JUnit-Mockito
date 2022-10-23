c041d7d6b6193d15b2afee9aae471bb3da5a7b5a
thinogueiras <thiagonogueira@hotmail.com.br>
2022-10-23 02:43:18 -0300

    Suíte de testes

---
+++ src/test/java/qa/thinogueiras/suite/SuiteTest.java
@@ -0,0 +1,10 @@
+package qa.thinogueiras.suite;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({"qa.thinogueiras.servicos"})
+public class SuiteTest {
+
+}
